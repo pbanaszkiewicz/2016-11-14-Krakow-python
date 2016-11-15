12c6794af645a18a6afe4bbcbc69fb61dac4965c
Piotr Banaszkiewicz <piotr@banaszkiewicz.org>
2016-11-15 15:32:03 +0100

    Implementacja sinusa

---
+++ trygonometria.py
@@ -0,0 +1,14 @@
+def sin(x):
+    if x == 90:
+        return 1
+    elif x == 60:
+        return (3 ** (0.5)) / 2
+    elif x == 45:
+        return (2 ** (0.5)) / 2
+    elif x == 30:
+        return 0.5
+    elif x == 0:
+        return 0
+    else:
+        return False
+
