97164415aca73e95658061273409844f3c49aa20
Piotr Banaszkiewicz <piotr@banaszkiewicz.org>
2016-11-15 15:43:00 +0100

    Implementacja cosinusa

--- trygonometria.py
+++ trygonometria.py
@@ -12,3 +12,17 @@ def sin(x):
     else:
         return False
 
+def cos(x):
+    if x == 90:
+        return 0
+    elif x == 60:
+        return 0.5
+    elif x == 45:
+        return (2 ** (0.5)) / 2
+    elif x == 30:
+        return (3 ** (0.5)) / 2
+    elif x == 0:
+        return 1
+    else:
+        return False
+
